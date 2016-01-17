f9f40a31af4f85970fc9d1fb264108e0891ff5a8
aistomin <andrej.istomin.ikeen@gmail.com>
2016-01-17 23:11:13 +0100

    Issue #3. Dummy class and test for it were added. dummy.txt files were removed.

---
+++ src/main/java/org/rising/dummy/Dummy.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2016, Istomin Andrei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.rising.dummy;
+
+/**
+ * Dummy class.
+ *
+ * @author Andrei Istomin (andrej.istomin.ikeen@gmail.com)
+ * @version $Id$
+ * @since 1.0
+ */
+public final class Dummy {
+
+    /**
+     * Dummy field.
+     */
+    private final transient String text;
+
+    /**
+     * Ctor.
+     * @param dum Dummy field.
+     */
+    public Dummy(final String dum) {
+        this.text = dum;
+    }
+
+    /**
+     * Dummy method.
+     * @return Dummy string.
+     */
+    public String execute() {
+        return this.text;
+    }
+}
