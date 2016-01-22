9522f9458c6e4b74a008b3a5c37314ae67aaf253
aistomin <andrej.istomin.ikeen@gmail.com>
2016-01-22 10:43:36 +0100

    Issue #49. RealUsers.java class was created. - puzzle for RealUsers.xml() was added. - test class with sample method was added.

---
+++ src/main/java/org/rising/jenkins/real/RealUsers.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2016, Istomin Andrei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.rising.jenkins.real;
+
+import org.apache.commons.lang3.NotImplementedException;
+import org.rising.jenkins.Users;
+
+/**
+ * Jenkins' users.
+ *
+ * @author Andrei Istomin (andrej.istomin.ikeen@gmail.com)
+ * @version $Id$
+ * @since 1.0
+ */
+public final class RealUsers implements Users {
+
+    /**
+     * Users XML representation.
+     *
+     * @return XML's string.
+     * @throws Exception If something goes wrong.
+     * @todo: Let's implement this method and solve Issue #50.
+     */
+    public String xml() throws Exception {
+        throw new NotImplementedException(
+            String.format(
+                "xml() method is not implemented for %s.",
+                this.getClass().getCanonicalName()
+            )
+        );
+    }
+}
